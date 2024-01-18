3f55e994941e1fe18923ec75fe6e6cbefa0b9529
smit <itsmit6879@gmail.com>
2024-01-18 15:41:44 +0530

    string function practice

---
+++ smit.php
@@ -0,0 +1,124 @@
+<?php
+//echo"smit chavda";
+
+//strlen
+
+// echo strlen("smit chavda");
+
+
+
+
+//str_replace($search, $replace, $subject):
+
+// $x="this is an apple , apple in red color"; 
+
+// echo str_replace("apple","mango",$x);
+
+// $find =["this","apple"];
+// $replace=["i","mango"];
+
+// echo str_replace($find,$replace,$x)
+
+
+
+
+// strpos($haystack, $needle):
+
+// $x="hey , i am smit !!! ";
+// echo strpos($x,"!");
+
+
+
+
+// substr($string, $start, $length):
+
+// $x="hey smit chavda";
+// echo substr($x,8);
+
+
+
+// strtolower($string):
+
+// $x="HEY I AM SMIT CHAVDA";
+// echo strtolower($x);
+
+
+
+// strtoupper($string):
+
+// $x="hey , my name is smit !!";
+// echo strtoupper($x)
+
+
+// $x="hey my name is smit";
+
+// echo $x."<br>";
+
+// echo trim($x,"ht");
+
+
+// $a=array('this','is','an','example');
+// echo implode(" ",$a);
+
+
+
+// $str = "Hello world. It's a beautiful day.";
+// print_r (explode(" ",$str));
+
+// echo str_repeat("smit",15);
+
+// echo strrev("my name is smit")
+
+// echo str_shuffle("my name is smit");
+
+
+
+// $a="smit chavda";
+
+// $array=str_split($a);
+
+// echo "<pre>";
+// print_r($array);
+// echo "</pre>";
+
+// echo str_word_count("chavda smit rahulbhai");
+
+
+// echo substr_replace("chavda smit ","disha",7);
+
+
+// echo ucfirst("hey i am smit");
+
+// echo ucwords("hey i am smit");
+
+// $a="smit";
+
+// echo str_pad($a,15,".");
+
+
+// $string1="smit";
+// $string2="hkasi";
+
+// echo strcoll($string1,$string2);
+
+
+$str = "i am smit chavda";
+echo htmlentities($str, ENT_COMPAT); // Will only convert double quotes
+echo "<br>";
+echo htmlentities($str, ENT_QUOTES); // Converts double and single quotes
+echo "<br>";
+echo htmlentities($str, ENT_NOQUOTES); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
---
+++ string_ex.php
@@ -0,0 +1,68 @@
+<?php
+
+// $firstname="smit";
+// $middlename="rahulbhai";
+// $surname="chavda";
+
+// echo $firstname." ".$middlename." ".$surname;
+
+
+// $text="Hello, World! How are you doing?";
+
+// $l=strlen($text);
+// echo "length of string is $l";
+
+// $e=strtolower($text);
+// echo($e);
+
+// echo strtoupper($text);
+
+// echo str_replace("how are you doing?","fine , thank you",$text);
+
+// echo substr($text,0,10);
+
+// echo substr($text,8);
+
+
+
+// // $sentence="The quick brown fox jumps over the lazy dog";
+
+// // echo substr_count($sentence,"cat");
+
+// // echo substr($sentence,0,20);
+
+
+// $name="john";
+// echo str_pad($name,10,"_",STR_PAD_LEFT);
+// echo str_pad($name,8,"*");
+
+
+// $quote="in three words i can sum up everything i've learned about life:it goes on";
+
+// echo str_word_count($quote);
+
+// echo strtolower($quote);
+
+// echo ucwords($quote);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+
+
+
