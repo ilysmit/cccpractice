9310853cc6bbc5d44c12c5db70a6fce9e5ec763a
smit <itsmit6879@gmail.com>
2024-01-22 14:22:28 +0530

    adding this file

---
+++ string_exercise_7.php
@@ -0,0 +1,23 @@
+<?php
+for($i=1; $i<=100; $i++){
+        if(
+            ($i%3==0) && ($i%5==0)
+        ){
+            echo "fizz buzz </br>";
+        } 
+        elseif($i%3==0)
+        {
+            echo "fizz </br>";
+        }
+        elseif($i%5==0)
+        {
+            echo "buzz </br>";
+        }
+        else
+        {
+            echo $i."</br>";
+        }
+
+    
+}
+?>
